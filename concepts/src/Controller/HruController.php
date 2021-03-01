463130d916efc554c2b5ef3f09ee7e24bbd137fb
DENIS Nicolas <denis.nicolas@hotmail.fr>
2021-03-01 16:49:54 +0100

    Mise à jour 01/03/2021

--- concepts/src/Controller/HruController.php
+++ concepts/src/Controller/HruController.php
@@ -5,16 +5,41 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HruController extends AbstractController
 {
     /**
-     * @Route("/hru", name="hru")
-     */
-    public function index(): Response
+     * @Route("/hru/{lien}", name="hru")
+    */
+    public function index(Request $request, $lien): Response
     {
+        $fichier = $lien;
+        $chemin='./../public/uploads/rubriques/';
+
+        //$monfichier = fopen($chemin.$fichier, 'r+');
+        //$ligne = fgets($monfichier);        
+        //fclose($monfichier);
+        $row =1;
+        if (($handle = fopen($chemin.$fichier, "r")) !== FALSE) {
+          while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+              $num = count($data);
+              echo "<p> $num champs à la ligne $row: <br /></p>\n";
+              $row++;
+              for ($c=0; $c < $num; $c++) {
+                  echo $data[$c] . "<br />\n";
+              }
+          }
+          fclose($handle);
+      }
         return $this->render('hru/index.html.twig', [
             'controller_name' => 'HruController',
+            'fichier'=>$fichier,
+            'num'=>$num,
+            'row'=>$row,
         ]);
     }
+
+    
+ 
 }
---
+++ concepts/src/Form/ProductType.php
@@ -0,0 +1,43 @@
+<?php
+// src/Form/ProductType.php
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            // ...
+            ->add('brochure', FileType::class, [
+                'label' => 'Brochure (PDF file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File()],
+            ])
+            // ...
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
